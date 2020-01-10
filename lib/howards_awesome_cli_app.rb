dbc9c0f17b1c6559d5c8ac482881b86a609c7de4
howardbdev <howarddevennish@gmail.com>
2020-01-10 12:01:41 -0500

    initial commit

---
+++ lib/howards_awesome_cli_app.rb
@@ -0,0 +1,6 @@
+require "howards_awesome_cli_app/version"
+
+module HowardsAwesomeCliApp
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/howards_awesome_cli_app/version.rb
@@ -0,0 +1,3 @@
+module HowardsAwesomeCliApp
+  VERSION = "0.1.0"
+end
